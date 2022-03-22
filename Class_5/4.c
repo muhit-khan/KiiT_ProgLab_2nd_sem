0ad16034e1afdffdabec505f09093d0e9b529223
Muhit Khan <muhitkhan_bd@outlook.com>
2022-03-22 23:07:38 +0530

    Class 5 update

--- Class_5/4.c
+++ Class_5/4.c
@@ -7,23 +7,10 @@ int main()
     scanf("%2d:%2d", &h1, &m1);
     printf("Enter second time\t:\t");
     scanf("%d:%d", &h2, &m2);
-    if(m1>=m2)
-    {
-        m = m1 - m2;
-    }
-    else
-    {
-        m = (m1+60) - m2;
-        h1 = h1-1;
-    }
-    if(h1>=h2)
-    {
-        h = h1 - h2;
-    }
-    else
-    {
-        h = h2 - h1;
-    }
+    m=m1-m2;
+    m=(m>=0)?(m):(m1+60-m2);
+    h=(m<0)?((h1-1)-h2):h1-h2;
+    h =(h<0)?(h+24):h;
     printf("Difference between times:\t%d:%d",h, m );
 
     return 0;
