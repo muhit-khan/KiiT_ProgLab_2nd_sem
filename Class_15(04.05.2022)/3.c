97ca1648de59d4d5ce619038122fad9e2ef7ea96
Muhit Khan <muhitkhan_bd@outlook.com>
2022-05-16 19:14:10 +0530

    Class_15 reupdate

--- Class_15(04.05.2022)/3.c
+++ Class_15(04.05.2022)/3.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int a = 20, b = 80, tem;
-    int *ap = &a, *bp = &b, *temp;
-    *temp = *ap;
-    *ap = *bp;
-    *bp = *temp;
+    int a = 11, b = 2;
+    int *pa=&a,*pb=&b,*temp;
+   
+    temp = pa;
+    pa = pb;
+    pb = temp;
 
-    printf("a = %d\n", *ap);
-    printf("b = %d", *bp);
+    printf("a = %d\n", *pa);
+    printf("b = %d\n", *pb);
 
     return 0;
 }
--- Class_15(04.05.2022)/7.c
+++ Class_15(04.05.2022)/7.c
@@ -3,16 +3,17 @@ int main()
 {
     int a[10];
     int m = 5;
-    for (int i = 0, j = 1, k = 50; i <= 10; i, j++, k -= j)
+    for (int i = 0, j = 0, k = 50; i < 10; i++, j++, k -= j)
     {
         if(i % 2)
             m += j;
         else
             m -= j;
-        a[m] = k;
+       
+        a[m-1] = k;
     }    
-    for (int k = 0; k < 10; ++k)
-        printf("%d\n", *(a + k));
+    for (int k = 0; k < 10; ++k){
+        printf("%d ", *(a + k));
+    }
     return 0;
 }
-//incomple
