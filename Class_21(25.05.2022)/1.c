d3b84828f5ed923ca7eab4d3074374f357bbd321
Muhit Khan <muhitkhan_bd@outlook.com>
2022-07-20 00:48:58 +0600

    Update 1.c

--- Class_21(25.05.2022)/1.c
+++ Class_21(25.05.2022)/1.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
- 
 struct student
 {
     int roll_no,marks;
     char name[25];
 }stud[100],t;
  
- void sortS(int n){
+void sortS(int n)
+{
  	int i,j;
- 	 for(i=0;i<n;i++)
+ 	for(i=0;i<n;i++)
     {
         for(j=0;j<n-1;j++)
         {
@@ -21,7 +21,7 @@ struct student
         }
     }
     
- }
+}
 void main()
 {
     int i,j,n;
@@ -33,7 +33,7 @@ void main()
         scanf("%d %s %d",&stud[i].roll_no,stud[i].name,&stud[i].marks);
     }
     
-   sortS(n);
+    sortS(n);
     printf("\nStudent info in terms of marks from highest to lowest\n");
     printf("\nROLL_NO\t\t\tNAME\t\t\tMARKS\n");
     printf("------------------------------------------------------\n");
